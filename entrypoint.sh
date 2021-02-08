8449cba57f324d1e10f24400063fb577114f3bb2
a z <a.z@noyb.fck>
2021-02-08 10:03:59 +0100

    added pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ cd stdc
 
 codespell --builtin clear,rare,code,names --ignore-words-list ths,dur,ois,tim,stdio  --interactive 0 --skip="./.git,./_prj_NucleoF411,./_prj_MKI109V3,./_prj_spc584b,./_bin,./_resources/STMems_Finite_State_Machine,./_resources/STMems_Machine_Learning_Core"
 
+cd /
+
+pwd
 
 astyle --style=otbs \
 --indent=spaces=2 \
