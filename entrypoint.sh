5e7cff60b701c338ce3f0916e72ed1bdf46546a3
a z <a.z@noyb.fck>
2021-02-08 10:55:57 +0100

    test 000

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,48 +3,57 @@
 git clone --recursive https://github.com/EmbALL/STMems_Standard_C_drivers.git stdc
 
 cd stdc
+if [ $1 != "SKIP" ]
+then
+  codespell --builtin clear,rare,code,names \
+  --ignore-words-list ths,dur,ois,tim,stdio \
+  --interactive 0 \ 
+  --skip="./.git,./_prj_NucleoF411, \
+          ./_prj_MKI109V3,./_prj_spc584b, \
+          ./_bin,./_resources/STMems_Finite_State_Machine, \
+          ./_resources/STMems_Machine_Learning_Core" 
+fi
 
-codespell --builtin clear,rare,code,names --ignore-words-list ths,dur,ois,tim,stdio  --interactive 0 --skip="./.git,./_prj_NucleoF411,./_prj_MKI109V3,./_prj_spc584b,./_bin,./_resources/STMems_Finite_State_Machine,./_resources/STMems_Machine_Learning_Core"
 
-pwd
-
-astyle --style=otbs \
---indent=spaces=2 \
---attach-closing-while \
---indent-switches \
---indent-labels \
---indent-preproc-define \
---indent-col1-comments \
---min-conditional-indent=0 \
---max-continuation-indent=60 \
---break-blocks=all \
---pad-comma \
---pad-oper \
---pad-header \
---delete-empty-lines \
---align-pointer=name \
---align-reference=name \
---break-closing-braces \
---break-one-line-headers \
---add-braces \
---attach-return-type \
---convert-tabs \
---max-code-length=70 \
---break-after-logical \
---pad-method-prefix \
---pad-return-type \
---pad-param-type \
---pad-method-colon=after \
---suffix=none \
---recursive ./*.c,*.h \
---exclude=_resources/STMems_Machine_Learning_Core \
---exclude=_resources/STMems_Finite_State_Machine \
---exclude=_prj_NucleoF411 \
---exclude=_prj_MKI109V3 \
---verbose \
---formatted \
---lineend=windows
-
+if [ $2 != "SKIP" ]
+then
+  astyle --style=otbs \
+  --indent=spaces=2 \
+  --attach-closing-while \
+  --indent-switches \
+  --indent-labels \
+  --indent-preproc-define \
+  --indent-col1-comments \
+  --min-conditional-indent=0 \
+  --max-continuation-indent=60 \
+  --break-blocks=all \
+  --pad-comma \
+  --pad-oper \
+  --pad-header \
+  --delete-empty-lines \
+  --align-pointer=name \
+  --align-reference=name \
+  --break-closing-braces \
+  --break-one-line-headers \
+  --add-braces \
+  --attach-return-type \
+  --convert-tabs \
+  --max-code-length=70 \
+  --break-after-logical \
+  --pad-method-prefix \
+  --pad-return-type \
+  --pad-param-type \
+  --pad-method-colon=after \
+  --suffix=none \
+  --recursive ./*.c,*.h \
+  --exclude=_resources/STMems_Machine_Learning_Core \
+  --exclude=_resources/STMems_Finite_State_Machine \
+  --exclude=_prj_NucleoF411 \
+  --exclude=_prj_MKI109V3 \
+  --verbose \
+  --formatted \
+  --lineend=windows
+fi
 
 echo "your input $1 $2"
 myvar="Hello"
