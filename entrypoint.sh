b46f4d359afeeb93196d528a57068ee94d7a6604
a z <a.z@noyb.fck>
2021-02-09 07:58:42 +0100

    test 007

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,42 +20,7 @@ fi
 
 if [ $3 != "SKIP" ]
 then
-  astyle --style=otbs \
-  --indent=spaces=2 \
-  --attach-closing-while \
-  --indent-switches \
-  --indent-labels \
-  --indent-preproc-define \
-  --indent-col1-comments \
-  --min-conditional-indent=0 \
-  --max-continuation-indent=60 \
-  --break-blocks=all \
-  --pad-comma \
-  --pad-oper \
-  --pad-header \
-  --delete-empty-lines \
-  --align-pointer=name \
-  --align-reference=name \
-  --break-closing-braces \
-  --break-one-line-headers \
-  --add-braces \
-  --attach-return-type \
-  --convert-tabs \
-  --max-code-length=70 \
-  --break-after-logical \
-  --pad-method-prefix \
-  --pad-return-type \
-  --pad-param-type \
-  --pad-method-colon=after \
-  --suffix=none \
-  --recursive ./*.c,*.h \
-  --exclude=_resources/STMems_Machine_Learning_Core \
-  --exclude=_resources/STMems_Finite_State_Machine \
-  --exclude=_prj_NucleoF411 \
-  --exclude=_prj_MKI109V3 \
-  --verbose \
-  --formatted \
-  --lineend=windows
+  astyle --options = $3
 fi
 
 echo "your input $1 $2 $3"
